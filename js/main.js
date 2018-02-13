55dfc105d6c3f676fd99a839be5db242213a18d9
Israel Moreira <israel@82web.com.br>
2018-02-13 09:40:06 -0300

    Tratando a descrição e os valores da tabela

--- js/main.js
+++ js/main.js
@@ -18,10 +18,29 @@ function getTotal(lista){
 function setList(lista){
   var table = '<thead><tr><th>Descrição</th><th>Quantidade</th><th>Valor</th><th>Ação</th></tr></thead><tbody>';
   for(var chave in lista){
-    table += '<tr><td>'+ lista[chave].desc +'</td><td>'+ lista[chave].quant +'</td><td>'+ lista[chave].valor +'</td><td>Editar | Excluir</td></tr>'
+    table += '<tr><td>'+ formatDesc (lista[chave].desc) +'</td><td>'+ lista[chave].quant +'</td><td>'+ formatValue(lista[chave].valor) +'</td><td>Editar | Excluir</td></tr>'
     table += '</tbody';
     document.getElementById('listaTabela').innerHTML = table;
   }
+}
+
+//FORMATANDO A DESCRIÇÃO DOS PRODUTOS
+function formatDesc(desc){
+   var str = desc.toLowerCase(); // deixa todos os textos em caixa baixa.
+   str = str.charAt(0).toUpperCase() + str.slice(1); // Pega o primeiro caracter do valor deixa em caixa alta e concatena com o restante da palavra.
+   return str;
+}
+
+//FORMATANDO O VALOR DOS PRODUTOS
+
+function formatValue(value){
+  // PEGANDO O VALOR TRANSFORMANDO EM FLOAT, INFORMANDO QUE EU QUERO DUAS CASAS DECIMAIS E TRANSFORMANDO NOVAMENTE EM STRING.
+  //SUBSTITUINDO O PONTO POR VÍRGULA EM FORMATO REAL
+  // ADICIONANDO O CIFRÃO
+  var str = parseFloat(value).toFixed(2) + "";
+  str = str.replace(".",",");
+  str = "R$ " + str;
+  return str;
 }
 
 setList(lista);
